cf0d61a287624606e05d5605986371c2d95aa655
shehzad002 <52923030+shehzad002@users.noreply.github.com>
2021-01-04 05:13:24 +0500

    Update colors.ts

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,22 +1,22 @@
-export const white = '#FFF'
-export const black = '#000'
+export const white = '#b3cde0'
+export const black = '#035b96'
 
 export const green = {
-  500: '#fff',
+  500: '#00d1810',
 }
 
 export const red = {
-  100: '#fff',
-  200: '#fff',
-  500: '#d16c00',
+  100: '#b3cde0',
+  200: '#b3cde0',
+  500: '#035b96',
 }
 
 export const grey = {
-  100: '#333',
-  200: '#0C0C0C',
-  300: '#0C0C0C',
-  400: '#fff',
-  500: 'green',
-  600: '#fff',
-  800: '#2d1e12',
+  100: '#b3cde0',
+  200: '#04396c',
+  300: '#b3cde0',
+  400: '#b3cde0',
+  500: '#b3cde0',
+  600: '#b3cde0',
+  800: '#b3cde0',
 }
